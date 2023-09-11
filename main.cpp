987c89b30645de5bd72be765b27b5257a0c66d8b
aaham <aaron.t.hamil@ou.edu>
2023-09-11 15:13:17 -0500

    function which computes the product 'multiplication' of all of a vector given as a parameter and returns the result.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,20 @@ double computeSum(const std::vector<double>& numbers) {
     return sum;
 }
 
+double computeProduct(const std::vector<double>& numbers) {
+    double product = 1.0;
+    for (double num : numbers)
+        product *= num;
+    return product;
+}
+
 int main() {
     std::vector<double> numbers = {1.5, 3.0, 4.5, 9.0, 10.5};
-    double result = computeSum(numbers);
+    double sum = computeSum(numbers);
+    double product = computeProduct(numbers);
 
-    std::cout << "The sum of the numbers is: " << result << '\n';
+    std::cout << "The sum of the numbers is: " << sum << '\n';
+    std::cout << "The product of the numbers is: " << product << '\n';
+
     return 0;
 }
